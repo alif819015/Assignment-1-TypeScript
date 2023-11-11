ce14fa9ffc3a2cef5bd628ff54a18a4a7f7ea5cf
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-11-11 19:57:58 +0600

    car and driver related info

--- Problem-Solving/src/problem5.ts
+++ Problem-Solving/src/problem5.ts
@@ -1,6 +1,27 @@
 {
 
-    
+    interface Car {
+        make: string;
+        model: string;
+        year: number;
+      }
+      
+      interface Driver {
+        name: string;
+        licenseNumber: string;
+      }
+      
+
+      const combineCarAndDriver = (car: Car, driver: Driver): { car: Car; driver: Driver } => {
+        return {car, driver};
+      }
+      
+      const carInfo: Car = { make: 'Toyota', model: 'Camry', year: 2023 };
+      const driverInfo: Driver = { name: 'Mahmud Hasan', licenseNumber: 'PH12345' };
+      
+      const combinedInfo = combineCarAndDriver(carInfo, driverInfo);
+      console.log(combinedInfo);
+      
 
 
 }   
